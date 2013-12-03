9e85032ddcb934aa486b67652e77c76e203b3d16
nakamura001 <polluxtk@gmail.com>
2013-12-03 20:55:34 +0900

    プログラム部分をコミット。

---
+++ Assets/HitTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+			return;
+			
+		Renderer renderer = hit.collider.renderer;
+		Texture2D tex = renderer.material.mainTexture as Texture2D;
+		Vector2 pixelUV = hit.textureCoord;
+		pixelUV.x *= tex.width;
+		pixelUV.y *= tex.height;
+				
+		int x = (int)Mathf.Floor(pixelUV.x);
+		int y = (int)Mathf.Floor(pixelUV.y);
+		Debug.Log( "Texture Name : " + hit.transform.gameObject.name + ", u=" + pixelUV.x + ", v=" + pixelUV.y + ", w=" + tex.width + ", h=" + tex.height + ", " + tex.GetPixel(x, y));
+	}
+}
